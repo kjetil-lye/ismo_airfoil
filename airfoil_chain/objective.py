1597c6339e00ee522d2a5248892e1963e6a69e81
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-04 14:15:41 +0200

    fixes

--- airfoil_chain/objective.py
+++ airfoil_chain/objective.py
@@ -9,12 +9,18 @@ class Objective(object):
         self.penalization_lift = penalization_lift
         self.penalization_area = penalization_area
 
-    def __call__(self, C_D, C_L, A):
+    def __call__(self, x):
+        C_D = x[1]
+        C_L = x[0]
+        A = x[2]
         return C_D/self.C_D_ref + \
             self.penalization_lift * np.maximum(0, 0.999 - C_L/self.C_L_ref) + \
             self.penalization_area * np.maximum(0, self.A_ref-A)
 
-    def grad(self, C_D, C_L, A):
+    def grad(self, x):
+        C_D = x[1]
+        C_L = x[0]
+        A = x[2]
         return np.array([1.0/self.C_D_ref,
                          (0.999 - C_L/self.C_L_ref > 0) * self.penalization_lift * max(0, 0.999 - C_L/self.C_L_ref),
                          (self.A_ref-A > 0) * self.penalization_area * max(0, self.A_ref-A)])
--- airfoil_chain/run.sh
+++ airfoil_chain/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-bash ../run_python_script.sh submit_airfoil.py --number_of_samples_per_iteration 2 2 2 2 "$@"
+bash ../run_python_script.sh submit_airfoil.py --number_of_samples_per_iteration 64 16 16 16 16 "$@"
