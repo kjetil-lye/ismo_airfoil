c8a9cb3b93cf8644f5b70cfc4f326580b7336d0f
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-18 23:17:53 +0200

    fixes

--- run_ensemble_default_integrated.sh
+++ run_ensemble_default_integrated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-source source_nuwtun.sh
-export PYTHONPATH=${PYTHONPATH}:$(pwd)/iterative_surrogate_optimization
+source source_for_nuwtun.sh
+export PYTHONPATH=${PYTHONPATH}:$(pwd)/iterative_surrogate_optimization:$(pwd)
 mkdir ensemble_output
 cd ensemble_output
 python ../ensemble_run/run_ensemble_integrated.py "$@"
