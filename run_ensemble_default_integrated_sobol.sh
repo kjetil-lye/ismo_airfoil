5ff8ae0139aa874f25e26350a4449b3bf38ed56c
Kjetil Olsen Lye <kjetil.o.lye@gmail.com>
2019-06-25 12:13:47 +0200

    script now actually runs sobol generator

--- run_ensemble_default_integrated_sobol.sh
+++ run_ensemble_default_integrated_sobol.sh
@@ -4,4 +4,4 @@ source source_for_nuwtun.sh
 export PYTHONPATH=${PYTHONPATH}:$(pwd)/iterative_surrogate_optimization:$(pwd)
 mkdir ensemble_output_sobol
 cd ensemble_output_sobol
-python ../ensemble_run/run_ensemble_integrated.py "$@"
+python ../ensemble_run/run_ensemble_integrated.py --generator sobol "$@"
